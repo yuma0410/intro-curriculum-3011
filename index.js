b47e351e5089b8669264d936698c72a96a1c360f
yuma0410 <115466706+yuma0410@users.noreply.github.com>
2022-12-05 12:32:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(req.headers['<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します'</h1></body></html>]);
   res.end();
 });
 const port = 8000;
